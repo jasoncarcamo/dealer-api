607f4463d877e9b41d5b2f065dacdbc2f99ff5b7
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2022-04-20 21:38:46 -0400

    Updated empoyee teble and .env file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/api-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/dealer-api',
     NODE_ENV: process.env.NODE_ENV || 'development',    
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
--- migrations/002.do.create_deals.sql
+++ migrations/002.do.create_deals.sql
@@ -4,7 +4,7 @@ CREATE TABLE deals (
     year INTEGER NOT NULL,
     make TEXT NOT NULL,
     model TEXT NOT NULL,
-    trim TEXT NOT NULL.
+    trim TEXT NOT NULL,
     vin TEXT NOT NULL,
     trade_in BOOLEAN DEFAULT FALSE NOT NULL,
     trade_year INTEGER,
@@ -14,5 +14,5 @@ CREATE TABLE deals (
     comments TEXT NOT NULL,
     write_up_date TIMESTAMP,
     deposit INTEGER NOT NULL,
-    employee INTEGER REFERENCES emplyess(id) NOT NULL
+    employee INTEGER REFERENCES employees(id) NOT NULL
 );
